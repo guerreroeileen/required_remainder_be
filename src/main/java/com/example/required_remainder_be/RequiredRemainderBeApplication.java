c67334e323e90e15f3233737540cca610462bad2
Eileen Guerrero <eileenguerrerogomez@gmail.com>
2025-06-26 10:01:29 -0500

    first commit

---
+++ src/main/java/com/example/required_remainder_be/RequiredRemainderBeApplication.java
@@ -0,0 +1,13 @@
+package com.example.required_remainder_be;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RequiredRemainderBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RequiredRemainderBeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/required_remainder_be/RequiredRemainderBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.required_remainder_be;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RequiredRemainderBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
